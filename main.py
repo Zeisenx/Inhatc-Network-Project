63157ee14f755f497d76ba2ffd65df46b39e4fb0
Zeisen <negativezeisen@gmail.com>
2020-12-15 05:03:33 +0900

    decode sample image via pyzbar

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import logging
+
 import requests
+from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-from bs4 import BeautifulSoup
-
 from content import token_id as telegramtoken
+
+import pyzbar.pyzbar as pyzbar
+import cv2
+
+# import picamera
 
 # Enable logging
 logging.basicConfig(
@@ -13,6 +18,28 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+def decode_image(image):
+    decoded_image = pyzbar.decode(image)
+    return decoded_image
+
+def test_sample_image():
+    image = cv2.imread('./content/sample_image.jpg')
+    decoded_image = decode_image(image)
+    for obj in decoded_image:
+        print(obj.type, obj.data)
+        print(getInfo(obj.data.decode('utf-8')))
+
+
+def takeShot(_time):
+    pass
+    # stream = io.BytesIO()
+    # with picamera.PiCamera() as camera:
+    #     camera.start_preview()
+    #     time.sleep(_time)
+    #     camera.capture(stream, format='jpeg')
+    # stream.seek(0)
+    # return stream
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -60,20 +87,25 @@ def set_timer(update: Update, context: CallbackContext) -> None:
 
 
 def getInfo(barcode_id):
-    url = 'http://www.gs1kr.org/Service/Service/appl/01.asp'
+    try:
+        url = 'http://www.gs1kr.org/Service/Service/appl/01.asp'
 
-    payload = {'MEMB_DIV': 1, 'CODE': barcode_id, 'CODE1': ''}
-    a = requests.post(url, data=payload)
+        payload = {'MEMB_DIV': 1, 'CODE': barcode_id, 'CODE1': ''}
+        a = requests.post(url, data=payload)
 
-    result_soup = BeautifulSoup(a.text, 'html.parser')
-    data = result_soup.find('table', {'class': ['odd', 'nofirst', 'tc']})
+        result_soup = BeautifulSoup(a.text, 'html.parser')
+        data = result_soup.find('table', {'class': ['odd', 'nofirst', 'tc']})
 
-    return data.select('tr > td')[1].get_text(), data.select('tr > td')[3].get_text()
+        return data.select('tr > td')[1].get_text(), data.select('tr > td')[3].get_text()
+    except:
+        return None
 
 
 def main():
     COLA_ID = '8801094082604'
     print(getInfo(COLA_ID))
+
+    test_sample_image()
 
     """Run bot."""
     # Create the Updater and pass it your bot's token.
@@ -88,7 +120,6 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", start))
     dispatcher.add_handler(CommandHandler("set", set_timer))
-    dispatcher.add_handler(CommandHandler("unset", unset))
 
     # Start the Bot
     updater.start_polling()
